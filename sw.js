401615168e48734bdda3692aa434a7f038fb6c42
JamesCalebWay <jcway324@gmail.com>
2025-07-17 19:18:35 -0400

    Template Setup

---
+++ sw.js
@@ -0,0 +1,17 @@
+// Repository Name
+var GHPath = '/PWA';
+
+// App prefix name
+var APP_PREFIX = 'pwa_';
+
+// ****Change version number any time any files are changed or else****
+var VERSION = 'version_00';
+
+var URLS = [
+    `${GHPath}/`,
+    `${GHPath}/index.html`,
+    `${GHPath}/styles/styleSheet.css`
+    `${GHPath}/scripts/app.js`
+]
+
+// https://christianheilmann.com/2022/01/13/turning-a-github-page-into-a-progressive-web-app/
