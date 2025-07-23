a91b270fdddee6c7d0d2b2fb22f446b717a852e6
JamesCalebWay <jcway324@gmail.com>
2025-07-23 16:52:07 -0400

    styles take 8

--- sw.js
+++ sw.js
@@ -5,12 +5,12 @@ var GHPath = '/PWA';
 var APP_PREFIX = 'pwa_';
 
 // ****Change version number any time any files are changed or else****
-var VERSION = 'version_0.0.0.6';
+var VERSION = 'version_0.0.0.7';
 
 var URLS = [
     `${GHPath}/`,
     `${GHPath}/index.html`,
-    `${GHPath}/stylesheet.css`
+    `${GHPath}/style.css`
     `${GHPath}/scripts/app.js`
 ]
 
